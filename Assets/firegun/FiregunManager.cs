d2a27d2b92fb73fd5d8925f7e984bc8b96b0e924
Bingha <foat3376@korea.ac.kr>
2022-11-03 03:47:39 +0900

    Create FireGun.prefab

--- Assets/firegun/FiregunManager.cs
+++ Assets/firegun/FiregunManager.cs
@@ -2,17 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class firegunmanager : MonoBehaviour
+public class FiregunManager : MonoBehaviour
 {
+    [SerializeField]
+    GameObject flameThrower;
+    [SerializeField]
+    GameObject attackRange;
+    [SerializeField]
+    bool mouseEvent = true;
+    ParticleSystem particleSystem1;
     // Start is called before the first frame update
     void Start()
     {
-        
+        particleSystem1 = flameThrower.GetComponent<ParticleSystem>();
+        StopFire();
     }
 
-    // Update is called once per frame
+    void FireFire()
+    {
+        particleSystem1.Play();
+        attackRange.SetActive(true);
+    }
+    void StopFire()
+    {
+        particleSystem1.Stop();
+        attackRange.SetActive(false);
+    }
     void Update()
     {
-        
+        if (!mouseEvent) return;
+
+        if (Input.GetKeyDown(KeyCode.Mouse0)) FireFire();
+        if (Input.GetKeyUp(KeyCode.Mouse0)) StopFire();
+
+
     }
 }
