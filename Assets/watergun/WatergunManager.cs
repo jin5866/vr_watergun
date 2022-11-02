5a67bf9de3ffdebe6287150180a7d7b24203672c
Bingha <foat3376@korea.ac.kr>
2022-11-03 02:23:31 +0900

    Create Water Gun.prefab

--- Assets/watergun/WatergunManager.cs
+++ Assets/watergun/WatergunManager.cs
@@ -6,67 +6,67 @@ using com.zibra.liquid.Manipulators;
 public class WatergunManager : MonoBehaviour
 {
     [SerializeField]
-    Transform zibraLiquid, zibraLiquidEmitter;
+    GameObject BallColider;
     [SerializeField]
-    GameObject LiquidColider;
+    Transform BallParent;
     [SerializeField]
-    float liquidPower = 0.15f;
+    int BallPerSecond = 5;
     [SerializeField]
     bool verbose = false;
     [SerializeField]
     bool implementMouseevent = true;
-    Vector3 coliderForce = new(0, 0, 750);
-
-    ZibraLiquidEmitter liquidEmitter;
+    public float ballSpeed = 30;
+    public Vector3 OriginVelocityNormal = Vector3.up;
+    bool ifFireing = false;
+    float lastBallCreationTime;
 
     // Start is called before the first frame update
     void Start()
     {
-        // If there's no Transform, It get Transform from getChild.
-        if(zibraLiquid == null || zibraLiquidEmitter == null)
-        {
-            zibraLiquid = transform.GetChild(0);
-            zibraLiquidEmitter = zibraLiquid.GetChild(0);
+    }
 
-            if (verbose) Debug.Log("Please attach zibraLiquid on Watergun Manager");
+    Vector3 Location()
+    {
+        float x = transform.localScale.x;
+        float y = transform.localScale.y;
+        float z = transform.localScale.z;
+        Vector3 a = new(Random.Range(-x,x)/2, Random.Range(-y, y) / 2, Random.Range(-z, z) / 2);
+        a = transform.rotation * a;
+        return a;
+    }
+    void CreateBall()
+    {
+        GameObject gameObject = !BallParent ? Instantiate(BallColider) : Instantiate(BallColider, BallParent);
+        Rigidbody rigidbody = gameObject.GetComponent<Rigidbody>();
+        gameObject.SetActive(true);
+        if (!gameObject || !rigidbody)
+        {
+            Debug.LogError("GameObject and Collider not be found.");
+            return;
         }
-        liquidEmitter = zibraLiquidEmitter.GetComponent<ZibraLiquidEmitter>();
-        if (!liquidEmitter)
-            Debug.LogError("Initialize Failed on WatergunManeger:29");
+        gameObject.transform.position = transform.position + Location();
+        rigidbody.velocity = this.gameObject.transform.rotation * OriginVelocityNormal * ballSpeed;
+        lastBallCreationTime = Time.realtimeSinceStartup;
     }
     public void FireWater()
     {
-        liquidEmitter.VolumePerSimTime = liquidPower;
+        ifFireing = true;
+        CreateBall();
         if (verbose) Debug.Log("Fire water.");
     }
     public void StopWater()
     {
-        liquidEmitter.VolumePerSimTime = 0;
+        ifFireing = false;
         if (verbose) Debug.Log("Stop firing water.");
     }
 
     // Update is called once per frame
     void Update()
     {
-        // If there's no emitter stop processing.
-        if (!liquidEmitter) return;
-
-        if(liquidEmitter.VolumePerSimTime != 0)
+        if (ifFireing && Time.realtimeSinceStartup - lastBallCreationTime > 1 / BallPerSecond)
         {
-            GameObject gameObject = Instantiate(LiquidColider);
-            gameObject.SetActive(true);
-            //Collider collider = gameObject.GetComponent<Collider>();
-            Rigidbody rigidbody = gameObject.GetComponent<Rigidbody>();
-            if(!gameObject || !rigidbody)
-            {
-                Debug.LogError("GameObject and Collider not be found.");
-                return;
-            }
-            gameObject.transform.position = zibraLiquidEmitter.position;
-
-            rigidbody.AddForce(coliderForce);
+            CreateBall();
         }
-
         // This is test feature.
         // If there's mouse event, please be turn off feature.
         if (implementMouseevent)
