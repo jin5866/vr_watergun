b36033328dd53686bcdaaa4efadddb56ac880b0d
jin5866 <hjl5866@naver.com>
2022-11-04 00:23:06 +0900

    Implement LevelMove

--- Assets/LevelMove.cs
+++ Assets/LevelMove.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelMove : MonoBehaviour
 {
@@ -8,16 +9,52 @@ public class LevelMove : MonoBehaviour
     [SerializeField]
     GameObject text;
 
+    [SerializeField]
+    Color ActivedColor;
+    [SerializeField]
+    Color InavtivedColor;
+    [SerializeField]
+    ParticleSystem Particle;
+
+    [SerializeField]
+    float rotateMinDist = 2.0f;
+
+    [SerializeField]
+    string LevelName = "";
+
+    [SerializeField]
+    float WaitTime = 5.0f;
+
+    [SerializeField]
+    float RemainTime = 0.0f;
+    bool IsEnter = false;
     private GameObject player;
     void Start()
     {
         player = FindObjectOfType<PlayerState>().gameObject;
+        SetParticleColor(false);
+        RemainTime = WaitTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        text.transform.LookAt(player.transform);
+
+        if (Vector3.Distance(gameObject.transform.position, player.transform.position) > rotateMinDist)
+        {
+            text.transform.LookAt(player.transform);
+        }
+
+        if (IsEnter)
+        {
+            RemainTime -= Time.deltaTime;
+        }
+
+        if (RemainTime <= 0)
+        {
+            ChangeLevel();
+        }
+        
     }
 
     private void OnTriggerEnter(Collider other)
@@ -25,7 +62,45 @@ public class LevelMove : MonoBehaviour
         if(other.gameObject == player)
         {
             Debug.Log("Trigger Enter");
+            SetParticleColor(true);
+            RemainTime = WaitTime;
+            IsEnter = true;
         }
+
+
         
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            Debug.Log("Trigger Out");
+            SetParticleColor(false);
+            RemainTime = WaitTime;
+            IsEnter = false;
+        }
+    }
+
+    private void SetParticleColor(bool active)
+    {
+        if (Particle)
+        {
+            if (active)
+            {
+                var main = Particle.main;
+                main.startColor = ActivedColor;
+            }
+            else
+            {
+                var main = Particle.main;
+                main.startColor = InavtivedColor;
+            }
+        }
+    }
+
+    private void ChangeLevel()
+    {
+        SceneManager.LoadScene(LevelName);
     }
 }
