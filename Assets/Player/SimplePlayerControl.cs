fc3ea99b206677f1a388644d41f6d099614fc0e1
jin5866 <hjl5866@naver.com>
2022-11-04 03:17:23 +0900

    player

--- Assets/Player/SimplePlayerControl.cs
+++ Assets/Player/SimplePlayerControl.cs
@@ -1,9 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
 
 public class SimplePlayerControl : MonoBehaviour
 {
+    [SerializeField]
+    SteamVR_Action_Boolean leftfire;
+    [SerializeField]
+    SteamVR_Action_Boolean rightfire;
+
+
+    [SerializeField]
+    GameObject watergunPrefab;
+    [SerializeField]
+    GameObject firegunPrefab;
+    [SerializeField]
+    Hand lefthand;
+    [SerializeField]
+    Hand righthand;
+
+    [SerializeField]
+    GameObject leftPosition;
+    [SerializeField]
+    GameObject RightPosition;
+
     [SerializeField]
     WatergunManager WatergunManager;
     [SerializeField]
@@ -15,6 +37,20 @@ public class SimplePlayerControl : MonoBehaviour
     {
         if(!WatergunManager) Debug.LogError("Please attach WatergunManager.");
         if(!firegunManager) Debug.LogError("Please attach FiregunManager.");
+
+        Hand.AttachmentFlags attachmentFlags = Hand.defaultAttachmentFlags & (~Hand.AttachmentFlags.SnapOnAttach) & (~Hand.AttachmentFlags.DetachOthers) ;
+
+        
+        GameObject watergun = Instantiate(watergunPrefab, RightPosition.transform);
+        GameObject firegun = Instantiate(firegunPrefab, leftPosition.transform);
+        WatergunManager = watergun.GetComponent<WatergunManager>();
+        firegunManager = firegun.GetComponent<FiregunManager>();
+
+        righthand.AttachObject(watergun, righthand.GetGrabStarting(), attachmentFlags);
+        lefthand.AttachObject(firegun, lefthand.GetGrabStarting(), attachmentFlags);
+
+        lefthand.SetVisibility(false);
+        righthand.SetVisibility(false);
     }
 
     // Update is called once per frame
@@ -26,20 +62,35 @@ public class SimplePlayerControl : MonoBehaviour
             {
                 firegunManager.Fire();
             }
+            else
+            {
+                firegunManager.StopFire();
+            }
             if (Input.GetKeyDown(KeyCode.Mouse1))
             {
                 WatergunManager.Fire();
             }
-        }
-            if (Input.GetKeyUp(KeyCode.Mouse0))
-            {
-                firegunManager.StopFire();
-            }
-            if (Input.GetKeyUp(KeyCode.Mouse1))
+            else
             {
                 WatergunManager.StopFire();
             }
-            {
         }
+        if (rightfire.GetState(SteamVR_Input_Sources.Any))
+        {
+            firegunManager.Fire();
+        }
+        else
+        {
+            firegunManager.StopFire();
+        }
+        if (rightfire.GetState(SteamVR_Input_Sources.Any))
+        {
+            WatergunManager.Fire();
+        }
+        else
+        {
+            WatergunManager.StopFire();
+        }
+
     }
 }
